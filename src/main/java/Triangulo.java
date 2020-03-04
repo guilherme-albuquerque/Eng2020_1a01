f433672b445eee32f5394dc266de34330887315b
Gabih-R <gabih.rodrigues2018@gmail.com>
2020-03-04 18:52:06 -0300

    criacao de classes

---
+++ src/main/java/Triangulo.java
@@ -0,0 +1,27 @@
+public class Triangulo  extends Poligono implements Perimetro{
+	private double l1, l2,l3;
+	
+	public Triangulo (double l1, double l2, double l3, double base, double altura) {
+		super (base, altura);
+		this.l1 = l1;
+		this.l2 = l2;
+		this.l3 = l3;
+		
+}
+		public Triangulo() {
+			
+		}
+		
+		public double area() {
+			return(getBase()*getAltura())/2;
+			
+				
+			}
+		@Override
+		public double perimetro() {
+			return l1+l2+l3;
+		}
+		
+		@Override
+		public String toString
+}
